676e13163308fc22c446ede8ca946a2dadf1abe1
tirtabayu16 <tirtaabayuu@gmail.com>
2022-04-14 14:12:09 +0700

    buat project

---
+++ backend-cicd/src/main/java/com/projectbackend/backendcicd/BackendCicdApplication.java
@@ -0,0 +1,13 @@
+package com.projectbackend.backendcicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendCicdApplication.class, args);
+	}
+
+}
---
+++ backend-cicd/src/test/java/com/projectbackend/backendcicd/BackendCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectbackend.backendcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
